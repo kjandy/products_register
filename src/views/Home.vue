6149fc2987b7d5573d0c8201342ffd2a71800341
koji <86970788+kjandy@users.noreply.github.com>
2022-04-29 11:34:42 +0900

    first_commit

---
+++ src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <h1>商品検索(タグ)<br>localStrage</h1>
+
+    <p><input type="text" v-model="keyword"></p>
+    <div class="search__wrapper">
+      <ul>
+        <li v-for="(p, index) in searchFiltered" :key="index">
+          {{ p.name }}
+        </li>
+      </ul>
+    </div><!-- /.search__wrapper -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      searchProducts: [],
+      keyword: ''
+    }
+  },
+  computed: {
+    searchFiltered() {
+      return this.searchProducts.filter(searchProduct => {
+        if(this.keyword) {
+          return searchProduct.selectedTags.includes(this.keyword);
+        }
+      })
+    }
+  },
+  mounted() {
+    // console.log(this.selectedTags);
+    if (localStorage.getItem('products')) {
+      try {
+        this.searchProducts = JSON.parse(localStorage.getItem('products'));
+      } catch(e) {
+        localStorage.removeItem('products');
+      }
+    }
+  },
+}
+</script>
+
+
+<style scoped>
+.search__wrapper {
+  background-color: rgb(207, 244, 253);
+  text-align: left;
+  padding: 40px 60px;
+}
+</style>
